0a22b262c1a1c276d289f0372f36ffab658eafb0
sahimsaami <sahimsaam@gmail.com>
2025-11-30 17:35:18 +0100

    Third Line Job Description

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -3,4 +3,6 @@ let count = 0;
 count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// Describe what line 3 is doing, in particular focus on what = is doing 
+// Line 3 is updating the value of the count variable by adding 1 to its current value. The = operator is an assignment operator that assigns the result of the expression on the right (count + 1) to the variable on the left (count).
+console.log(count);
